9584e8a7046ba3582786fc32befcba04bb4ddf72
David Freeman <david@d3.codes>
2025-10-22 22:48:34 -0500

    hide miniplayer

--- Off The Spots/Views/Player/PlayerAccessoryView.swift
+++ Off The Spots/Views/Player/PlayerAccessoryView.swift
@@ -18,13 +18,51 @@ struct PlayerAccessoryView: View {
     var body: some View {
         if selectedSong != nil {
             Group {
-//                switch tabViewBottomAccessoryPlacement {
-//                case .expanded:
+                switch tabViewBottomAccessoryPlacement {
+                case .expanded:
                     HStack {
                         VStack(alignment: .leading) {
                             Text(selectedSong!.name)
                                 .font(.headline)
-//                                .bold()
+                            
+                            Text(selectedSong!.selectedTrack.name)
+                                .font(.subheadline)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            if(player.isPlaying) {
+                                player.pause()
+                            } else {
+                                player.play()
+                            }
+                        }, label: {
+                            if(player.isPlaying) {
+                                Image(systemName: "pause.fill")
+                                    .font(.title2)
+                                    .tint(.primary)
+                            } else {
+                                Image(systemName: "play.fill")
+                                    .font(.title2)
+                                    .tint(.primary)
+                            }
+                        })
+
+                        Button(action: { player.skip(seconds: -15) }, label: {
+                            Image(systemName: "15.arrow.trianglehead.counterclockwise")
+                                .font(.title2)
+                                .tint(.primary)
+                        })
+                        .padding(.leading, 10)
+                    }
+                default:
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(selectedSong!.name)
+                                .font(.headline)
             //                    .matchedGeometryEffect(id: "name", in: namespace)
                             
                             Text(selectedSong!.selectedTrack.name)
@@ -53,49 +91,8 @@ struct PlayerAccessoryView: View {
                                     .tint(.primary)
                             }
                         })
-//
-//                        Button(action: { player.skip(seconds: -15) }, label: {
-//                            Image(systemName: "15.arrow.trianglehead.counterclockwise")
-//                                .font(.title2)
-//                                .tint(.primary)
-//                        })
-//                        .padding(.leading, 10)
                     }
-//                default:
-//                    HStack {
-//                        VStack(alignment: .leading) {
-//                            Text(selectedSong!.name)
-//                                .font(.headline)
-//            //                    .matchedGeometryEffect(id: "name", in: namespace)
-//                            
-//                            Text(selectedSong!.selectedTrack.name)
-//                                .font(.subheadline)
-//            //                    .matchedGeometryEffect(id: "track", in: namespace)
-//                        }
-//                        .frame(maxWidth: .infinity, alignment: .leading)
-//                        
-//                        
-//                        Spacer()
-//                        
-//                        Button(action: {
-//                            if(player.isPlaying) {
-//                                player.pause()
-//                            } else {
-//                                player.play()
-//                            }
-//                        }, label: {
-//                            if(player.isPlaying) {
-//                                Image(systemName: "pause.fill")
-//                                    .font(.title2)
-//                                    .tint(.primary)
-//                            } else {
-//                                Image(systemName: "play.fill")
-//                                    .font(.title2)
-//                                    .tint(.primary)
-//                            }
-//                        })
-//                    }
-//                }
+                }
             }
             .padding(.horizontal)
             .contentShape(Capsule())
--- Off The Spots/Views/Set Lists/SetListsView.swift
+++ Off The Spots/Views/Set Lists/SetListsView.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct SetListsView: View {
+    @Binding var hideMiniPlayer: Bool
+    
     var body: some View {
         NavigationStack {
             Text("Set Lists")
                 .navigationTitle("Set Lists")
                 .toolbar {
                     ToolbarItem(placement: .topBarLeading) {
-                        NavigationLink(destination: SettingsView(), label: {Image(systemName: "gearshape") })
+                        NavigationLink(destination: SettingsView(hideMiniPlayer: $hideMiniPlayer), label: {Image(systemName: "gearshape") })
                     }
                 }
+                .onAppear { hideMiniPlayer = false }
         }
     }
 }
 
 #Preview {
-    SetListsView()
+    struct SetListsView_Preview: View {
+        @State private var hideMiniPlayer: Bool = false
+        
+        var body: some View {
+            SetListsView(hideMiniPlayer: $hideMiniPlayer)
+        }
+    }
+    
+    return SetListsView_Preview()
 }
