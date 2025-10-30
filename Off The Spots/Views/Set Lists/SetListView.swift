e3d6a6beae8d00233ac6a86202188434a20c3db2
David Freeman <david@d3.codes>
2025-10-30 12:08:08 -0500

    view and edit set list

---
+++ Off The Spots/Views/Set Lists/SetListView.swift
@@ -0,0 +1,96 @@
+//
+//  SetListView.swift
+//  Off The Spots
+//
+//  Created by David Freeman on 10/30/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct SetListView: View {
+    @Query(sort: [SortDescriptor(\Song.order)]) private var songs: [Song]
+    
+    @State var setList: SetList
+    
+    @State private var selection = Set<Song.ID>()
+    @State private var presentEditSetListSheet: Bool = false
+    
+    var body: some View {
+        Group {
+            List(selection: $selection) {
+                ForEach(setList.songs, id: \.self) { songId in
+                    if let song = songs.first(where: { $0.id == songId }) {
+                        VStack(alignment: .leading) {
+                            Text(song.name)
+                                .font(.title2)
+                                .tint(.primary)
+                                .multilineTextAlignment(.leading)
+                            
+                            HStack {
+                                Text("\(Image(systemName: "music.note.square.stack.fill")) \(song.tracks.count)")
+                                    .font(.footnote)
+                                    .tint(.primary)
+                                
+                                if song.sheetMusic != nil {
+                                    Divider()
+                                    
+                                    Image(systemName: "music.pages.fill")
+                                        .font(.footnote)
+                                        .tint(.primary)
+                                }
+                            }
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                }
+                .onMove(perform: moveSongs)
+                .onDelete(perform: deleteSongs)
+            }
+        }
+        .navigationTitle(setList.name)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: { presentEditSetListSheet = true }, label: { Image(systemName: "pencil") })
+            }
+        }
+        .sheet(isPresented: $presentEditSetListSheet) {
+            EditSetListView(setList: $setList)
+                .interactiveDismissDisabled(true)
+        }
+    }
+    
+    private func deleteSongs(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                setList.songs.remove(at: index)
+            }
+        }
+    }
+
+    private func moveSongs(offsets: IndexSet, destination: Int) {
+        withAnimation {
+            setList.songs.move(fromOffsets: offsets, toOffset: destination)
+        }
+    }
+}
+
+#Preview {
+    let setList = SetList(name: "Set List Name", songs: [])
+    let container: ModelContainer = {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try! ModelContainer(for: Song.self, configurations: config)
+        for i in 1..<10 {
+            let track = Track(name: "Track 1", file: nil)
+            let song = Song(name: "Song \(i)", tracks: [track], selectedTrack: track, sheetMusic: nil)
+            setList.songs.append(song.id)
+            container.mainContext.insert(song)
+        }
+        return container
+    }()
+
+    NavigationStack {
+        SetListView(setList: setList)
+            .modelContainer(container)
+    }
+}
