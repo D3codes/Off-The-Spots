1188f14377804cbb7624d603267895d23457ba3c
David Freeman <david@d3.codes>
2025-11-07 11:47:59 -0600

    removed useless check

--- Off The Spots/Views/Player/PlayerAccessoryView.swift
+++ Off The Spots/Views/Player/PlayerAccessoryView.swift
@@ -17,107 +17,105 @@ struct PlayerAccessoryView: View {
     @ObservedObject var player: AudioHelper
 
     var body: some View {
-        if selectedSong != nil {
-            Group {
-                switch tabViewBottomAccessoryPlacement {
-                case .expanded:
-                    HStack {
-                        VStack(alignment: .leading, spacing: 0) {
-                            MarqueeText(
-                                text: selectedSong!.name,
-                                font: UIFont.preferredFont(forTextStyle: .headline),
-                                leftFade: 16,
-                                rightFade: 16,
-                                startDelay: 3
-                            )
-                            
-                            MarqueeText(
-                                text: selectedSong!.selectedTrack.name,
-                                font: UIFont.preferredFont(forTextStyle: .subheadline),
-                                leftFade: 16,
-                                rightFade: 16,
-                                startDelay: 3
-                            )
+        Group {
+            switch tabViewBottomAccessoryPlacement {
+            case .expanded:
+                HStack {
+                    VStack(alignment: .leading, spacing: 0) {
+                        MarqueeText(
+                            text: selectedSong!.name,
+                            font: UIFont.preferredFont(forTextStyle: .headline),
+                            leftFade: 16,
+                            rightFade: 16,
+                            startDelay: 3
+                        )
+                        
+                        MarqueeText(
+                            text: selectedSong!.selectedTrack.name,
+                            font: UIFont.preferredFont(forTextStyle: .subheadline),
+                            leftFade: 16,
+                            rightFade: 16,
+                            startDelay: 3
+                        )
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        if(player.isPlaying) {
+                            player.pause()
+                        } else {
+                            player.play()
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            if(player.isPlaying) {
-                                player.pause()
-                            } else {
-                                player.play()
-                            }
-                        }, label: {
-                            if(player.isPlaying) {
-                                Image(systemName: "pause.fill")
-                                    .font(.title2)
-                                    .tint(.primary)
-                            } else {
-                                Image(systemName: "play.fill")
-                                    .font(.title2)
-                                    .tint(.primary)
-                            }
-                        })
-
-                        Button(action: { player.skip(seconds: -15) }, label: {
-                            Image(systemName: "15.arrow.trianglehead.counterclockwise")
+                    }, label: {
+                        if(player.isPlaying) {
+                            Image(systemName: "pause.fill")
                                 .font(.title2)
                                 .tint(.primary)
-                        })
-                        .padding(.leading, 10)
-                    }
-                default:
-                    HStack {
-                        VStack(alignment: .leading, spacing: 0) {
-                            MarqueeText(
-                                text: selectedSong!.name,
-                                font: UIFont.preferredFont(forTextStyle: .headline),
-                                leftFade: 16,
-                                rightFade: 16,
-                                startDelay: 3
-                            )
-                            
-                            MarqueeText(
-                                text: selectedSong!.selectedTrack.name,
-                                font: UIFont.preferredFont(forTextStyle: .subheadline),
-                                leftFade: 16,
-                                rightFade: 16,
-                                startDelay: 3
-                            )
+                        } else {
+                            Image(systemName: "play.fill")
+                                .font(.title2)
+                                .tint(.primary)
                         }
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                    })
+
+                    Button(action: { player.skip(seconds: -15) }, label: {
+                        Image(systemName: "15.arrow.trianglehead.counterclockwise")
+                            .font(.title2)
+                            .tint(.primary)
+                    })
+                    .padding(.leading, 10)
+                }
+            default:
+                HStack {
+                    VStack(alignment: .leading, spacing: 0) {
+                        MarqueeText(
+                            text: selectedSong!.name,
+                            font: UIFont.preferredFont(forTextStyle: .headline),
+                            leftFade: 16,
+                            rightFade: 16,
+                            startDelay: 3
+                        )
                         
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            if(player.isPlaying) {
-                                player.pause()
-                            } else {
-                                player.play()
-                            }
-                        }, label: {
-                            if(player.isPlaying) {
-                                Image(systemName: "pause.fill")
-                                    .font(.title2)
-                                    .tint(.primary)
-                            } else {
-                                Image(systemName: "play.fill")
-                                    .font(.title2)
-                                    .tint(.primary)
-                            }
-                        })
+                        MarqueeText(
+                            text: selectedSong!.selectedTrack.name,
+                            font: UIFont.preferredFont(forTextStyle: .subheadline),
+                            leftFade: 16,
+                            rightFade: 16,
+                            startDelay: 3
+                        )
                     }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        if(player.isPlaying) {
+                            player.pause()
+                        } else {
+                            player.play()
+                        }
+                    }, label: {
+                        if(player.isPlaying) {
+                            Image(systemName: "pause.fill")
+                                .font(.title2)
+                                .tint(.primary)
+                        } else {
+                            Image(systemName: "play.fill")
+                                .font(.title2)
+                                .tint(.primary)
+                        }
+                    })
                 }
             }
-            .padding(.horizontal)
-            .contentShape(Capsule())
-            .onTapGesture { presentPlayerSheet = true }
-            .id(player.isPlaying)
         }
+        .padding(.horizontal)
+        .contentShape(Capsule())
+        .onTapGesture { presentPlayerSheet = true }
+        .id(player.isPlaying)
     }
 }
 
