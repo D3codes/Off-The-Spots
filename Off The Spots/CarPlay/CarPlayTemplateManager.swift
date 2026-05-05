f8fd2875cbc4bcaec13eba996b646347dfc03777
David Freeman <david@d3.codes>
2026-05-04 19:39:17 -0500

    refresh carplay on song change

--- Off The Spots/CarPlay/CarPlayTemplateManager.swift
+++ Off The Spots/CarPlay/CarPlayTemplateManager.swift
@@ -13,6 +13,7 @@ final class CarPlayTemplateManager: NSObject {
     let container: ModelContainer
     let modelContext: ModelContext
     let interfaceController: CPInterfaceController
+    private var selectedSongObserver: NSObjectProtocol?
 
     init(interfaceController: CPInterfaceController) {
         self.interfaceController = interfaceController
@@ -27,9 +28,31 @@ final class CarPlayTemplateManager: NSObject {
         CPNowPlayingTemplate.shared.isAlbumArtistButtonEnabled = true
         CPNowPlayingTemplate.shared.add(self)
         self.setNowPlayingButtons()
+        if let selectedSongObserver {
+            NotificationCenter.default.removeObserver(selectedSongObserver)
+        }
+        selectedSongObserver = NotificationCenter.default.addObserver(
+            forName: .audioHelperSelectedSongDidChange,
+            object: AudioHelper.sharedController,
+            queue: .main
+        ) { [weak self] _ in
+            Task { @MainActor in
+                self?.refreshNowPlayingForSelectedSong()
+            }
+        }
     }
     
     func disconnect() -> Void {
+        if let selectedSongObserver {
+            NotificationCenter.default.removeObserver(selectedSongObserver)
+        }
+        selectedSongObserver = nil
         AudioHelper.sharedController.pause()
+    }
+
+    private func refreshNowPlayingForSelectedSong() {
+        CPNowPlayingTemplate.shared.isAlbumArtistButtonEnabled = true
+        CPNowPlayingTemplate.shared.isUpNextButtonEnabled = AudioHelper.sharedController.selectedSetList != nil
+        setNowPlayingButtons()
     }
 }
